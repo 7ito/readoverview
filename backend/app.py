8d57aaffcb87675a8aa0e39d0b378fc39abab17f
7ito <justin@justintom.com>
2025-03-12 22:21:19 +0800

    Added translation to api

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from chinese import ChineseAnalyzer
+import translators as ts
+import ast
 
 # venv activate command: . .venv/bin/activate
 # run command: flask --app app --debug run
@@ -12,44 +14,32 @@ analyzer = ChineseAnalyzer()
 def status_check():
     return "Server is running..."
 
-@app.route("/test")
-def chinese_test():
-    result = analyzer.parse("我爱看书")
-    result.pprint()
-    return result.tokens()
-
+# TODO: Check if input text is Chinese and return error if not
+# res not being ordered is solved by looping through tokens array and using each token as dict key
 @app.route('/parse', methods=['POST'])
 def parse():
     data = request.json
     input_sentence = data.get('sentence', '')
     if not input_sentence:
         return jsonify({'error': 'No input provided'}), 400
-    
+
+    # Check if input is Chinese
     try: 
         parsed = analyzer.parse(input_sentence)
-        print(parsed.pformat())
-        return jsonify({'parsed': parsed.pformat()})
+
+        data = ast.literal_eval(parsed.pformat())
+        res = {}
+        for entry in data['parsed']:
+            item = {
+                'definitions': entry['dict_data'][0]['definitions'],
+                'kind': entry['dict_data'][0]['kind'],
+                'match': entry['dict_data'][0]['match'],
+                'pinyin': entry['dict_data'][0]['pinyin']
+            }
+            res[entry['token'][0]] = item
+
+        translation = ts.translate_text(input_sentence, 'google', 'zh', 'en')
+
+        return jsonify({'parsed': res, 'translation': translation, 'tokens': parsed.tokens(), 'raw': data})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
-# {'我': [
-#     {
-#         'definitions': ['I', 'me', 'my'],
-#         'kind': 'Simplified',
-#         'match': '我',    
-#         'pinyin': ['wo3']
-#     }
-# ],
-# '爱': [{'definitions': ['to love',
-#                        'to be fond of',
-#                        'to like',
-#                        'affection',
-#                        'to be inclined (to do sth)',
-#                        'to tend to (happen)'],
-#        'kind': 'Simplified',
-#        'match': '愛',
-#        'pinyin': ['ai4']}],
-# '看书': [{'definitions': ['to read', 'to study'],
-#         'kind': 'Simplified',
-#         'match': '看書',
-#         'pinyin': ['kan4', 'shu1']}]}

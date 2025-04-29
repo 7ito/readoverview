9461f89fd46e4a09affe641fb11d325409c0bf77
7ito <justin@justintom.com>
2025-04-30 00:06:09 +0800

    Touch and mobile first implementation, switched to streaming approach, favicon

--- backend/express/app.js
+++ backend/express/app.js
@@ -11,8 +11,8 @@ const PORT = 5000;
 const SYSTEM_PROMPT = `Instructions: 
 You are a Mandarin language expert. You will be given a Mandarin sentence and it's English translation. Your job is to segment the sentence into its words (词语), and provide the pinyin reading and English definition for each word in the context of the sentence
 Important: 
-Pinyin should be formatted like this: 'ni3 hao3' (你好), 'ta1' (他), 'nu:3' (女)
-Make sure there is a space between pinyin: e.g. 'ni3 hao3' not 'ni3hao3'
+Pinyin should be formatted using a number at the end to note tone: 'ni3 hao3' (你好), 'ta1' (他), 'nu:3' (女)
+With words (词语) of 2 or more characters, always ensure there is a space between pinyin: e.g. 'ni3 hao3' NOT 'ni3hao3'
 
 Format:
 Format response as JSON
@@ -39,7 +39,7 @@ Do not include any markdown formatting to denote a JSON string.
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://hanzilens.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
@@ -54,17 +54,89 @@ app.listen(PORT, () => {
 });
 
 app.post("/parse", validateInputIsChinese, async (req, res) => {
+  const { validatedText } = req;
   try {
-    const { validatedText } = req;
-    if (!validatedText) {
-      res.status(400).json({ error: "No input provided" });
-    }
     const translation = await translate(validatedText, { from: "zh", to: "en" });
-    const llmPrediction = await llmRequest(validatedText, translation);
+    const userPrompt = `Sentence: ${validatedText}\nTranslation: ${translation}`;
+    const model = "qwen/qwen-2.5-72b-instruct:free";
 
-    return res.status(200).json({ translation: translation, parsed: llmPrediction });
-  } catch (e) {
-    return res.status(500).json({ error: e.message });
+    const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENROUTER_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: model,
+        messages: [
+          {
+            role: "system",
+            content: SYSTEM_PROMPT,
+          },
+          {
+            role: "user",
+            content: userPrompt,
+          },
+        ],
+        stream: true,
+        response_format: {
+          type: 'json_schema',
+          json_schema: {
+            name: 'sentence_response',
+            strict: true,
+            schema: {
+              type: "object",
+              properties: {
+                "segments": {
+                  type: "array",
+                  items: {
+                    "token": {
+                      type: "string",
+                    },
+                    "pinyin": {
+                      type: "string",
+                    },
+                    "definition": {
+                      type: "string",
+                    },
+                  },
+                  required: ["token", "pinyin", "definition"],
+                }
+              }
+            },
+            required: ["segments"],
+          },
+        },
+      })
+    });
+
+    console.log("Streaming response...");
+
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const translationMetadataChunk = JSON.stringify({ metadata: { translation } }) + '\n';
+    res.write(`data: ${translationMetadataChunk}`);
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        console.log("End of stream");
+        res.end();
+        break;
+      }
+
+      const chunk = decoder.decode(value, { stream: true });
+      console.log("Raw chunk", chunk);
+      res.write(chunk);
+    }
+  } catch (error) {
+    console.error('Error streaming response:', error);
+    return res.status(500).json({ error: error.message });
   }
 });
 
@@ -159,65 +231,6 @@ const _recursiveSegmentImpl = async (segment) => {
   return [segment];
 };
 
-const llmRequest = async (sentence, translation) => {
-  const userPrompt = `Sentence: ${sentence}\nTranslation: ${translation}`;
-  const model = "qwen/qwen-2.5-72b-instruct:free";
-
-  const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${process.env.OPENROUTER_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      model: model,
-      messages: [
-        {
-          role: "system",
-          content: SYSTEM_PROMPT,
-        },
-        {
-          role: "user",
-          content: userPrompt,
-        },
-      ],
-      response_format: {
-        type: 'json_schema',
-        json_schema: {
-          name: 'sentence_response',
-          strict: true,
-          schema: {
-            type: "object",
-            properties: {
-              "segments": {
-                type: "array",
-                items: {
-                  "token": {
-                    type: "string",
-                  },
-                  "pinyin": {
-                    type: "string",
-                  },
-                  "definition": {
-                    type: "string",
-                  },
-                },
-                required: ["token", "pinyin", "definition"],
-              }
-            }
-          },
-          required: ["segments"],
-        },
-      },
-    })
-  });
-
-  const data = await response.json();
-  const result = data.choices[0].message.content;
-  console.log(result);
-  return(JSON.parse(result));
-}
-
 // Test case: 你喜欢这个味道吗 simplified
 // Traditional: 你喜歡這個味道嗎。
 // 你有光明的未来。
@@ -236,4 +249,4 @@ const llmRequest = async (sentence, translation) => {
 // Qwen72B: 8.63, 5.31, 7.9, 5.35, 5.98
 
 // 李白在蜀中長大，二十四歲開始出蜀漫遊各地，力圖獲得官員的舉薦而入仕，但事與願違多次碰壁，四十二歲時李白因身為高士而名動京師，獲唐玄宗徵召，出任翰林供奉，成為宮廷詩人，開展李白人生最光輝的時期，但兩年後李白即因被中傷和排擠而請辭，繼續踏上他的旅程。
-// Qwen72B Free: 1:03.47, 1:11.1,
+// Qwen72B Free: 1:03.47, 1:11.1, 1:17.4
--- frontend/readassist/src/App.jsx
+++ frontend/readassist/src/App.jsx
@@ -1,115 +1,212 @@
 import { useState, useEffect, useRef } from "react";
-import GridLoader from "react-spinners/GridLoader";
 import Ciyu from "./components/Ciyu";
-import { ArrowLeft, CircleHelp } from 'lucide-react';
+import { ArrowLeft, CircleHelp } from "lucide-react";
 import InfoModal from "./components/InfoModal";
 import { hasChineseText } from "./utils/Utils";
+import CircleLoader from "react-spinners/CircleLoader";
+import { IncompleteJsonParser } from "incomplete-json-parser";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isPreview, setIsPreview] = useState(false);
-  const [parsedData, setParsedData] = useState([]);
+  const [segments, setSegments] = useState([]);
   const [translation, setTranslation] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isValid, setIsValid] = useState(false);
+  const [charCount, setCharCount] = useState(0);
+  const [isCharLimitExceeded, setIsCharLimitExceeded] = useState(false);
   const editableRef = useRef(null);
+  const CHAR_LIMIT = 150;
 
   useEffect(() => {
     setIsValid(hasChineseText(userInput, 0.25));
+    setCharCount(userInput.length);
 
     if (!editableRef.current) return;
 
-    if (editableRef.current.innerHTML === '<br>') {
-      editableRef.current.innerHTML = '';
+    if (editableRef.current.innerHTML === "<br>") {
+      editableRef.current.innerHTML = "";
     }
   }, [userInput]);
 
   useEffect(() => {
-    const hasVisited = localStorage.getItem('hasVisited');
+    const hasVisited = localStorage.getItem("hasVisited");
     if (!hasVisited) {
       setIsModalOpen(true);
-      localStorage.setItem('hasVisited', 'true');
+      localStorage.setItem("hasVisited", "true");
     }
   }, []);
 
   const sendParse = async () => {
-    if (!isValid) {
-      return;
-    }
-
-    setIsLoading(true);
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/parse`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sentence: userInput }),
-    });
-    const data = await response.json();
-    setParsedData(data.parsed);
-    setTranslation(data.translation);
-    setIsLoading(false);
+    if (!isValid) return;
+    setSegments([]);
+    setTranslation("");
+    let contentBuffer = "";
     setIsPreview(true);
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/parse`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sentence: userInput }),
+      });
+
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+      let buffer = '';
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        buffer += decoder.decode(value, { stream: true });;
+
+        while (true) {
+            const lineEnd = buffer.indexOf('\n');
+            if (lineEnd === -1) break;
+
+            const line = buffer.slice(0, lineEnd).trim();
+            buffer = buffer.slice(lineEnd + 1);
+
+            if (line.startsWith('data: ')) {
+                const data = line.slice(6);
+
+                if (data === '[DONE]') {
+                    setSegments(JSON.parse(contentBuffer).segments);
+                    break;
+                }
+
+                try {
+                    const parsed = JSON.parse(data);
+
+                    if (parsed.metadata) {
+                        setTranslation(parsed.metadata.translation);
+                        continue;
+                    }
+
+                    if (parsed.choices && parsed.choices[0]?.delta?.content) {
+                        contentBuffer += parsed.choices[0].delta.content;
+
+                        const partialParsed = IncompleteJsonParser.parse(contentBuffer);
+                        console.log(partialParsed);
+
+                        if (partialParsed && partialParsed.segments && Array.isArray(partialParsed.segments)) {
+                            setSegments(partialParsed.segments);
+                        }
+                    }
+                } catch (e) {
+                    console.error('Error parsing JSON stream:', e);
+                }
+            }
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching streamed response:', error);
+    } finally {
+      setIsLoading(false);
+      console.log(segments);
+    }
   };
 
   const resetPage = () => {
     setUserInput("");
     setIsPreview(false);
-  }
+  };
+
+  const handleInput = (e) => {
+    const text = e.currentTarget.textContent;
+    if (text.length <= CHAR_LIMIT) {
+      setUserInput(text);
+      setIsCharLimitExceeded(false);
+    } else {
+      e.currentTarget.textContent = text.slice(0, CHAR_LIMIT);
+      setIsCharLimitExceeded(true);
+      setUserInput(text.substring(0, CHAR_LIMIT));
+    }
+  };
 
   return (
-    <div className="root-container flex bg-red-300 min-w-screen min-h-screen items-center justify-center">
-      {isLoading ? (
-        <div className="flex flex-col items-center justify-center text-lg">
-          <span className="pb-[2em]">Breaking it down...</span>
-          <GridLoader size={40} loading={isLoading} />
-          <span className="pt-[2em]">Please allow up to 60 seconds or more depending on the length of the sentence!</span>
-          <span className="pt-2">Click the icon at the top right for more information.</span>
-        </div>
-      ) : (
-          <>
-            {isPreview ? (
-              <div className="inner-container bg-red-300 flex flex-col items-center justify-center mx-[5em]">
-                <button className="absolute top-0 left-0 focus:outline-0 bg-red-300 px-[0.6em] py-[1.2em] cursor-pointer" onClick={() => resetPage()}><ArrowLeft /></button>
-                <div className="text-4xl text-black text-center pb-[2em]">{translation}</div>
-                <div className="flex items-start justify-center flex-wrap pt-3">
-                  {parsedData.segments.map((entry) => (
-                    <Ciyu
-                      key={entry.token}
-                      text={entry.token}
-                      pinyin={entry.pinyin.split(" ")}
-                      definition={entry.definition}
-                    />
-                  ))}
-                </div>
-              </div>
-            ) : (
-              <div className="inner-container bg-red-400 w-[90vw] h-[90vh] rounded-md flex flex-col items-center justify-center">
-                <div
-                  ref={editableRef}
-                  className="flex items-center justify-center w-full h-full p-4 text-4xl text-white text-center focus:outline-0 leading-normal resize-none"
-                  onInput={(e) => setUserInput(e.currentTarget.textContent)}
-                  contentEditable="plaintext-only"
-                  data-placeholder="Paste a Chinese sentence here!"
-                >
-                </div>
+    <div className="root-container flex flex-col bg-red-300 min-w-screen min-h-screen items-center justify-center">
+      <div>
+        <CircleHelp
+          onClick={() => setIsModalOpen(true)}
+          className="absolute right-1 top-1"
+        />
+      </div>
+        {isPreview ? (
+          <div className="inner-container bg-red-300 flex flex-col items-center justify-center mx-[5em]">
+            <button
+              className="absolute top-0 left-0 focus:outline-0 bg-red-300 px-[0.6em] py-[1.2em] cursor-pointer"
+              onClick={() => resetPage()}
+            >
+              <ArrowLeft />
+            </button>
+            <div className="text-xl lg:text-4xl text-black text-center py-[2em]">
+              {translation}
+            </div>
+            <div className="flex items-start justify-center flex-wrap pt-3">
+              {segments.map((entry) => (
+                <Ciyu
+                  key={entry.token}
+                  text={entry.token}
+                  pinyin={entry.pinyin ? entry.pinyin.split(" ") : [""]}
+                  definition={entry.definition}
+                />
+              ))}
+            </div>
+            {isLoading && <CircleLoader />}
+          </div>
+        ) : (
+          <div className="inner-container bg-red-400 w-[90vw] h-[90vh] rounded-md flex flex-col items-center justify-center overflow-auto">
+            <div
+              ref={editableRef}
+              className="flex items-center justify-center w-full h-full p-4 text-xl lg:text-4xl text-white text-center focus:outline-0 leading-normal resize-none whitespace-pre-wrap break-all"
+              onInput={handleInput}
+              contentEditable="plaintext-only"
+              data-placeholder="Paste a Chinese sentence here!"
+            ></div>
 
-                {!isValid && userInput.length > 1 && (
-                  <div className="text-xl text-red-600">
-                    Please ensure at least 25% of text is Chinese characters
-                  </div>
-                )}
-      
-                {userInput.length > 0 && <button onClick={sendParse} className="text-[1.2em] font-bold px-[0.6em] py-[1.2em] cursor-pointer">Go</button>}
+            {!isValid && userInput.length > 1 && (
+              <div className="text-sm lg:text-xl text-red-600 text-center">
+                Please ensure at least 25% of text is Chinese characters
               </div>
             )}
-          </>
-      )}
-      <CircleHelp onClick={() => setIsModalOpen(true)} className="fixed right-3 top-3" />
-      <InfoModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        
-      </InfoModal>
+
+            {isCharLimitExceeded && (
+              <div className="text-sm lg:text-xl text-red-600 text-center">
+                Maximum character limit exceeded (150 characters)
+              </div>
+            )}
+
+            {/* Character count display */}
+            <div
+              className="text-base text-gray-600 lg:text-lg py-2"
+              style={{
+                color: charCount > CHAR_LIMIT ? 'red' : '#4a5565'
+              }}
+            >
+              {charCount}/{CHAR_LIMIT}
+            </div>
+
+            {userInput.length > 0 && (
+              <button
+                onClick={sendParse}
+                className="text-[1.2em] font-bold px-[0.6em] py-[1.2em] cursor-pointer"
+              >
+                Go
+              </button>
+            )}
+          </div>
+        )}
+
+      <InfoModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      ></InfoModal>
     </div>
   );
 }
